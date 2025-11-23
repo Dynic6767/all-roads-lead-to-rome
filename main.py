504f5cd89212a15e6faa237d7a17764409e3c408
Dynic6767 <palatovdaniil38@gmail.com>
2025-11-23 15:11:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
+import telebot # библиотека telebot
+from config import token # импорт токена
 
-import telebot
+bot = telebot.TeleBot(token) 
 
+banned_words = ['амогус', 'сигма']
 
-bot = telebot.TeleBot("8497488826:AAEMr6T9OLsgdSRTkMCNvecxuxaooA2tg8M")
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
-user_states = {}
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'сдрасте мордасте, напишите сигма или амогу и вас забанят')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
-@bot.message_handler(commands=['start','help'] )
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я Telegram бот. Я отвечаю на много вопросов. Вот мои командды: /hello , /bye.")
-
-@bot.message_handler(commands=['hello'])
-def send_hello(message):
-    bot.reply_to(message, "Привет! Как дела?")
-    user_states[message.chat.id] = 'waiting_for_feedback'  
-
-@bot.message_handler(func=lambda message: message.chat.id in user_states and user_states[message.chat.id] == 'waiting_for_feedback')
-def handle_response(message):
-    if message.text.lower() == "хорошо":
-        bot.reply_to(message, "Очень хорошо слышать")
-        user_states.pop(message.chat.id)
-    elif message.text.lower() == "плохо":
-        bot.reply_to(message, "Извинте, это очень грустно слышать")
-        user_states.pop(message.chat.id)
-
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    bot.reply_to(message, "Пока! Удачи!")
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def check_banned_words(message):
+    text = message.text.lower()
+    for word in banned_words:
+        if word in text:
+            try:
+                bot.ban_chat_member(message.chat.id, message.from_user.id)
+                bot.reply_to(message, f"{message.from_user.first_name} был забанен за запрещённые слова.")
+            except Exception as e:
+                bot.reply_to(message, "нельзя забанить админа")
+            break
 
-if __name__ == '__main__':
-    print("Бот запущен...")
-    bot.infinity_polling()
+bot.infinity_polling(none_stop=True)
